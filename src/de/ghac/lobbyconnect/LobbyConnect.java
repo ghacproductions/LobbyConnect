0f6201595a9d659d98807c16021e5ba3737646cb
ghacproductions <simon.leiss93@gmail.com>
2014-07-25 13:45:45 +0200

    Initial commit.

---
+++ src/de/ghac/lobbyconnect/LobbyConnect.java
@@ -0,0 +1,31 @@
+package de.ghac.lobbyconnect;
+
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class LobbyConnect extends Plugin{
+    private BungeeCord bc;
+    @Override
+    public void onLoad()
+    {
+        //static
+        
+        bc=BungeeCord.getInstance();
+        bc.getLogger().info("[LobbyConnect] Loaded.");
+        
+    }
+        
+    @Override
+    public void onEnable(){
+
+     bc.getLogger().info("Enabled LobbyConnect.");
+     /* Register Listener */
+     }
+    
+    @Override
+    public void onDisable(){
+     bc.getLogger().info("Disabled LobbyConnect.");
+    }
+        
+
+}
